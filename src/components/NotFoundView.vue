66a02708a98774b988eb24da262d616c5a13642b
yashhmt <yash.hmt@outlook.com>
2026-03-11 12:26:34 +0530

    feat: update favicon and enhance Not Found page with error link

--- src/components/NotFoundView.vue
+++ src/components/NotFoundView.vue
@@ -4,7 +4,8 @@
 
 <template>
   <div class="container">
-    <h1 class="text-2xl font-bold mb-4">Page Not Found</h1>
+    <h1 class="text">Page Not Found</h1>
+    <a href="https://en.wikipedia.org/wiki/HTTP_404" class="link" target="_blank" rel="noopener noreferrer">Error 404</a>
   </div>
 </template>
 
@@ -12,5 +13,19 @@
 .container {
   background-color: #3b3b3b;
   padding: 2rem;
+  border-radius: 8px;
+}
+
+.text {
+  color: #fff;
+  font-size: 1.5rem;
+  text-align: center;
+}
+
+.link {
+  display: block;
+  margin-top: 1rem;
+  text-align: center;
+  color: #61dafb;
 }
 </style>
